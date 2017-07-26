fe19c120a7784b05d6efafed5a07a673a6290e59
stonysong <greefsong@163.com>
2017-07-26 22:51:48 +0800

    p
    p

---
+++ EShuiPlat.Plugs/Events/EventTest1AddEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using EShuiPlat.Core.Events;
+using EShuiPlat.Entity.EventEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShuiPlat.Plugs.Events
+{
+    public class EventTest1AddEventHandler : IEvent2Params<EventTest, EventTest1>
+    {
+        public void Handle(EventTest eventargs, EventTest1 eventparams)
+        {
+            
+            eventparams.order.Price = eventparams.order.Price + eventargs.order.Price;
+        }
+    }
+}
---
+++ EShuiPlat.Plugs/Events/EventTestAddEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using EShuiPlat.Core.Events;
+using EShuiPlat.Entity.EventEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShuiPlat.Plugs.Events
+{
+    public class EventTestAddEventHandler : IEvent1Param<EventTest>
+    {
+        public void Handle(EventTest eventargs)
+        {
+
+           
+            // eventargs.order.VersionNumber=10;
+            
+        }
+    }
+}
