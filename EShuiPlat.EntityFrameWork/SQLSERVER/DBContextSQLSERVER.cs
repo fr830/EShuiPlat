d65ecd3767508a49e60d5c9d3052cc94e685ae2a
stonysong <greefsong@163.com>
2017-07-26 22:40:06 +0800

    t
    t

---
+++ EShuiPlat.EntityFrameWork/SQLSERVER/DBContextSQLSERVER.cs
@@ -0,0 +1,18 @@
+﻿using EShuiPlat.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShuiPlat.EntityFrameWork.SQLSERVER
+{
+  public   class DBContextSQLSERVER:DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=TestDB;Integrated Security=True");
+        }
+    }
+}
