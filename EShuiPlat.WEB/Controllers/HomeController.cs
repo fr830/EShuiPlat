59ad4553ebac44e837722e09b119dccc70774d3c
stonysong <greefsong@163.com>
2017-07-26 22:54:43 +0800

    r
    r

---
+++ EShuiPlat.WEB/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EShuiPlat.Core.Events;
+using EShuiPlat.Entity;
+using EShuiPlat.EntityFrameWork.SQLSERVER;
+
+namespace EShuiPlat.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public Order AddPrice(Order ord)
+        {
+            ord.Price += 200;
+            return ord;
+        }
+        public IActionResult Index()
+        {
+            DBContextSQLSERVER dbc = new DBContextSQLSERVER();
+            Order md = new Order();
+            md.Price = 100;
+            md.BookNo = "123";
+            dbc.Orders.Add(md);
+            dbc.SaveChanges();
+            BusinessLink<Order> bus = new BusinessLink<Order>();
+            bus.BusinessFunc += AddPrice;
+            bus.BusinessFunc += AddPrice;
+            bus.BusinessFunc += AddPrice;
+            bus.BusinessFunc += AddPrice;
+            bus.BusinessFunc += AddPrice;
+            bus.BusinessFunc += AddPrice;
+            
+          var pp=  bus.On_BusinessFunc(md);
+            var p = md;
+           // string xmlstr = p.ToSQL();
+            string str = "";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
